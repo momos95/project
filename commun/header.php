cd8cb96f76b9c16d13a1b531dc09f054877c14bd
Flo <rozotte.florian@gmail.com>
2015-12-07 15:07:35 +0100

    Modif
    Header modifier

--- commun/header.php
+++ commun/header.php
@@ -1,3 +1,10 @@
+<?php
+/********************************************************
+ * Fichier header : commun à tous les utilsateurs       *
+ * Sert d'entete de nos page (header) , affiche le menu *
+ * selon le type de profil.                             *
+ ********************************************************/
+?>
 
 
 
@@ -9,63 +16,85 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-        <?php 
-            if(isset($_SESSION['is_user'])){?>
-                <li><a href="accueil.php" class = "navbar-brand">Business Expenses</a></li>
-            <?php }
-            else{?>
-                <li><a href="index.php" class = "navbar-brand">Business Expenses</a></li>
-            <?php }    
-        ?>
+        
+       <?php 
+        /************************************************
+         * Lien de l'application selon la situation :   *
+         *                                              *
+         * Si on est connecté ---> vers accueil         *
+         * Sinon : ----------> vers index               *
+         * **********************************************/
+
+        if(isset($_SESSION['is_user'])){?>
+            <li><a href="/BusinessExpenses/accueil.php" class = "navbar-brand">Business Expenses</a></li>
+        <?php }
+        else{?>
+            <li><a href="/BusinessExpenses/index.php" class = "navbar-brand">Business Expenses</a></li>
+        <?php }    
+    ?>
                 
         </div>
         
     <div class="navbar-collapse collapse">
 <?php
-//Une fois connectÃ© : on a pas besoin de Ã§a.
-    if(!isset($_SESSION['is_user'])){?>
+
+    /*******************************************************
+     * PARTIE COMMUNE : MENU AVANT CONNEXION               *
+     *                                                     *
+     *******************************************************/
+
+    if(!isset ($_SESSION['is_user']) || !(isset($_SESSION['is_user']) && $_SESSION['is_user'] )){
+        ?>
         <ul class="nav navbar-nav">
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="apropos.php">A Propos</a></li>
-            <li><a href="contacts.php">Contacts</a></li>
+            <li><a href="/BusinessExpenses/index.php">Accueil</a></li>
+            <li><a href="/BusinessExpenses/apropos.php">A Propos</a></li>
+            <li><a href="/BusinessExpenses/contacts.php">Contacts</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="/BusinessExpenses/login.php">Connexion</a></li>
         </ul>
 
     <?php }
             
-            //Si on est connectÃ© : possibilitÃ© de voir notre note de frais et de changer les param.
-            if(isset ($_SESSION['is_user']) && $_SESSION['is_user']){?>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="">Mes Notes de Frais</a></li>
-                    <li><a href="">ParamÃ¨tres</a></li>
-                    <li><a href="">Statisques</a></li>
+    /**************************************************************************************
+     * PARTIE : Utilisateur connecté.                                                     *
+     * Cette Partie est commune à tous les types de profil                                *
+     * Possibilité de voir notre note de frais, de changer les param et de se déconnecter.*
+     **************************************************************************************/
+     
+    if(isset ($_SESSION['is_user']) && $_SESSION['is_user']){?>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="/BusinessExpenses/">Mes Notes de Frais</a></li>
+            <li><a href="/BusinessExpenses/">Paramètres</a></li>
+            <li><a href="/BusinessExpenses/controllers/logoutController.php">Déconnexion</a></li>
+        </ul>
+     <?php }
 
-                    <li><a href="controllers/logoutController.php">DÃ©connexion</a></li>
+    /*********************************************************
+     * PARTIE MENU ADMIN   : id = 3                          *
+     *                                                       *
+     * Pour Que l'admin puisse ajouter un nouvel Utilisateur.*
+     * Faire des manipulations sur les tables de la base et  *
+     * voir les stastiques des frais des employés            *
+     *********************************************************/
 
-                    <!--<li><a href="../../controllers/logoutController.php">DÃ©connexion</a></li> !-->
+    if(isset($_SESSION['is_user']) && $_SESSION['user_profil'] == 'admin'){
+     ?>
+        <ul class="nav navbar-nav navbar-right">
+             <li><a href="/BusinessExpenses/views/admin/usersCheck.php">Administration</a></li>
+             <li><a href="/BusinessExpenses/">Statisques</a></li>
+        </ul>
+    <?php }
+    
+    if(isset($_SESSION['is_user']) && $_SESSION['user_profil'] == 'manager'){
+     ?>
+        <ul class="nav navbar-nav navbar-right">
+             <li><a href="">Mes Agents</a></li>
+        </ul>
+    <?php }
 
-                </ul>
-            <?php }
-            
-            //Pour que notre uitlisateur puisse se connecter
-            if(!isset ($_SESSION['is_user']) || !(isset($_SESSION['is_user']) && $_SESSION['is_user'] )){
-                ?>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="login.php">Connexion</a></li>
-                </ul>
-            <?php }
-            
-            //Pour Que l'admin puisse ajouter un nouvel Utilisateur.
-            if(isset($_SESSION['is_user']) && $_SESSION['user_profil'] == 3){
-             ?>
-                <ul class="nav navbar-nav navbar-right">
-                     <!--<li><a href="inscription.php">Utilisateurs</a></li>
-                     <li><a href="">Managers</a></li>
-                     <li><a href="">CatÃ©gories</a></li>
-                     <li><a href="">Devises</a></li>-->
-                     <li><a href="views/admin/usersCheck.php">Administration</a></li>
-                </ul>
-            <?php }
         ?>
+
     </div>
         
   </div>
